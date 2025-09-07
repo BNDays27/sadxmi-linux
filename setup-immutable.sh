6acbb9f91eaab34b7242f35bc0ac7d6f13e17921
BNDays27 <brionical@proton.me>
2025-09-07 11:29:57 +0100

    changed the linux one slightly, as well as added an immutable one for devices like the Steam Deck or for other devices

--- setup-immutable.sh
+++ setup-immutable.sh
@@ -4,38 +4,21 @@ icon_path="./samm.png"
 desktop_files=${XDG_DATA_HOME:-$HOME/.local/share}/applications
 icon_files=${XDG_DATA_HOME:-$HOME/.local/share}/icons/hicolor/256x256/apps
 
-# checks to see if all of the dependencies are installed
-if ! command -v protontricks &> /dev/null
-then
-    echo "Protontricks isn't installed, please install that through your distro's package manager or through pipx"
+appid=com.github.Matoking.protontricks
+if ! flatpak info "$appid" >/dev/null 2>&1; then
+    echo "Protontricks isnt installed, please install the flatpak before continuing"
     exit 0
-fi
-
-if ! command -v yad &> /dev/null
-then
-    echo "YAD isn't installed, please install that through your distro's package manager"
-    exit 0
-fi
-
-if ! command -v curl &> /dev/null
-then
-    echo "curl isn't installed, please install that through your distro's package manager"
-    exit 0
-fi
-
-if ! command -v zenity &> /dev/null
-then
-    echo "Zenity isn't installed, please install that through your distro's package manager"
-    exit 0
+else
+    echo "Protontricks is installed."
 fi
 
 # this is the actual installer
-echo "Entering setup"
-yad --width=750 --height=100 --info --title="$name Installation" --window-icon="$icon_path" --image="$icon_path" --text="Please select the directory where Sonic Adventure DX is installed" --button="OK:1" --button="Cancel:0"
-  if [ $? -eq 0 ]; then
-  echo "Script exited by user"
-  exit 0
-fi
+echo "entering setup"
+zenity --width=750 --height=100 --info --title="$name Installation" --text="Please select the directory where Sonic Adventure DX is installed"
+#if [ $? -eq 0 ]; then
+#  echo "Script exited by user"
+#  exit 0
+#fi
 
 selected_folder=$(zenity --file-selection --directory --title "select the directory where Sonic Adventure DX is installed")
 
@@ -50,7 +33,7 @@ fi
 
 # Downloads and launches the SADX mod installer, the rest of it should be handled by that
  curl https://dcmods.unreliable.network/owncloud/data/PiKeyAr/files/Setup/sadx_setup.exe --output "$selected_folder/sadx_setup.exe"
- protontricks-launch --appid 71250 "$selected_folder/sadx_setup.exe"
+ flatpak run --command=protontricks-launch com.github.Matoking.protontricks --appid 71250 "$selected_folder/sadx_setup.exe"
  
  # Checks if the mod manager was installed
  if [[ -f $selected_folder/SAModManager.exe ]]; then echo "SA Mod Manager has been installed"; 
@@ -62,11 +45,12 @@ fi
  fi
 
 # makes the .desktop entry for 1-click installs and easy opening in whatever application launcher you have
-echo "[Desktop Entry]
-Exec=protontricks-launch --appid 71250 '$selected_folder/SAModManager.exe' %u
+echo "#!/usr/bin/env xdg-open
+[Desktop Entry]
+Exec=flatpak run --command=protontricks-launch com.github.Matoking.protontricks --appid 71250 '$selected_folder/SAModManager.exe' %u
 GenericName=A new mod manager for the Sonic Adventure games.
-Icon=samm
 Keywords=hedgehog;mod;loader;manager;sonic
+Icon=samm
 Categories=Game;
 MimeType=x-scheme-handler/sadxmm;x-scheme-handler/sa2mm
 Name=Sonic Adventure Mod Manager
